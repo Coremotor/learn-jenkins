e624decf190c5cfc7033e9e5b5905812f19aa0c7
Андрей Демаков <andrei.demakov@travelline.ru>
2023-11-08 08:57:31 +0300

    fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import {useEffect, useState} from 'react'
 import './App.css'
 
 function App() {
+  const [user, setUser] = useState<any>(null)
+  useEffect(() => {
+    fetch('http://localhost:1234/api/user')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setUser(data);
+      });
+    }, [])
   return (
     <div>
       <div>Test !!!</div>
@@ -8,6 +19,9 @@ function App() {
       <div>VITE_SOME_KEY: {import.meta.env.VITE_SOME_KEY}</div>
       <div>VITE_TOKEN: {import.meta.env.VITE_TOKEN}</div>
       <div>CUSTOM_SOME_KEY: {import.meta.env.CUSTOM_SOME_KEY}</div>
+      <div>user</div>
+      <div>FirstName: {user?.firstName}</div>
+      <div>LastName: {user?.lastName}</div>
     </div>
   )
 }
